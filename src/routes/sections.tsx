ec80e29fc840858e4045de71cf03a262fa61a930
Manaf Hijazi <mhijazi@beyond.ai>
2024-10-13 16:41:58 +0300

    hoy fix

--- src/routes/sections.tsx
+++ src/routes/sections.tsx
@@ -1,10 +1,13 @@
 import { lazy, Suspense } from 'react';
 import { Outlet, Navigate, useRoutes } from 'react-router-dom';
+
 import Box from '@mui/material/Box';
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+
 import { varAlpha } from 'src/theme/styles';
 import { AuthLayout } from 'src/layouts/auth';
 import { DashboardLayout } from 'src/layouts/dashboard';
+
 import PrivateRoute from './components/PrivateRoute';
 
 // ----------------------------------------------------------------------
@@ -22,61 +25,61 @@ export const Page404 = lazy(() => import('src/pages/page-not-found'));
 // ----------------------------------------------------------------------
 
 const renderFallback = (
-    <Box display="flex" alignItems="center" justifyContent="center" flex="1 1 auto">
-        <LinearProgress
-            sx={{
-                width: 1,
-                maxWidth: 320,
-                bgcolor: (theme) => varAlpha(theme.vars.palette.text.primaryChannel, 0.16),
-                [`& .${linearProgressClasses.bar}`]: { bgcolor: 'text.primary' },
-            }}
-        />
-    </Box>
+  <Box display="flex" alignItems="center" justifyContent="center" flex="1 1 auto">
+    <LinearProgress
+      sx={{
+        width: 1,
+        maxWidth: 320,
+        bgcolor: (theme) => varAlpha(theme.vars.palette.text.primaryChannel, 0.16),
+        [`& .${linearProgressClasses.bar}`]: { bgcolor: 'text.primary' },
+      }}
+    />
+  </Box>
 );
 
 export function Router() {
-    return useRoutes([
-        {
-            path: '/',
-            element: (
-                <AuthLayout>
-                    <SignInPage />
-                </AuthLayout>
-            ),
-            index: true,
-        },
-        {
-            element: (
-                <PrivateRoute>
-                    <DashboardLayout>
-                        <Suspense fallback={renderFallback}>
-                            <Outlet />
-                        </Suspense>
-                    </DashboardLayout>
-                </PrivateRoute>
-            ),
-            children: [
-                { path: 'dashboard', element: <LinksPage /> },
-                { path: 'home', element: <HomePage /> },
-                { path: 'patient', element: <PatientPage /> },
-                { path: 'approvals', element: <ApprovalsPage /> },
-            ],
-        },
-        {
-            path: 'sign-in',
-            element: (
-                <AuthLayout>
-                    <SignInPage />
-                </AuthLayout>
-            ),
-        },
-        {
-            path: '404',
-            element: <Page404 />,
-        },
-        {
-            path: '*',
-            element: <Navigate to="/404" replace />,
-        },
-    ]);
+  return useRoutes([
+    {
+      path: '/',
+      element: (
+        <AuthLayout>
+          <SignInPage />
+        </AuthLayout>
+      ),
+      index: true,
+    },
+    {
+      element: (
+        <PrivateRoute>
+          <DashboardLayout>
+            <Suspense fallback={renderFallback}>
+              <Outlet />
+            </Suspense>
+          </DashboardLayout>
+        </PrivateRoute>
+      ),
+      children: [
+        { path: 'dashboard', element: <LinksPage /> },
+        { path: 'home', element: <HomePage /> },
+        { path: 'patient', element: <PatientPage /> },
+        { path: 'approvals', element: <ApprovalsPage /> },
+      ],
+    },
+    {
+      path: 'sign-in',
+      element: (
+        <AuthLayout>
+          <SignInPage />
+        </AuthLayout>
+      ),
+    },
+    {
+      path: '404',
+      element: <Page404 />,
+    },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
+  ]);
 }
